92303f1b64babadc42a0a093183ac74e97def32d
Victor Hoang <vthoang@msn.com>
2021-02-17 23:37:49 -0800

    adding lesson 5 homework

--- lesson03/exercise-three.js
+++ lesson03/exercise-three.js
@@ -3,3 +3,13 @@ let computerParts = ['CPU', 'GPU', 'RAM', 'PSU', 'Motherboard', 'Storage'];
 computerParts.forEach(function(i) {
   console.log(computerParts.indexOf(i) + "," + i );
 })
+
+// for (let i = 0; i < Array.length; i++) {
+//   console.log(i, computerParts[i])
+// }
+// alternative way 2
+
+// computerParts.forEach((food, index) => {
+//   console.log(index, food);
+// })
+// alternative way 3
